e852736f3b6409258d199e06083b6e96fa6462e6
smmfirouz <mahdiar67@gmail.com>
2017-05-07 16:06:20 +0430

    rename

--- src/model/ServerInfo.java
+++ src/model/ServerInfo.java
@@ -2,4 +2,20 @@ package model;
 
 public class ServerInfo {
 
+private	String ip;
+private	int port;
+public String getIp() {
+	return ip;
+}
+public void setIp(String ip) {
+	this.ip = ip;
+}
+public int getPort() {
+	return port;
+}
+public void setPort(int port) {
+	this.port = port;
+}
+	
+	
 }
